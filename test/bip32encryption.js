46a72f65d39f418f4462e76e64088781ecf21bac
John Kozan <john@kozan.sk.ca>
2015-04-29 16:22:35 -0600

    implement make wallet unencrypted

---
+++ test/bip32encryption.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var Bip32Encryption = require('../');
+var chai = require('chai');
+var assert = require('assert');
+var should = chai.should();
+var expect = chai.expect;
+
+var fixtures = require('./fixtures');
+
+describe('BIP32 encryption', function() {
+
+  var bip32e;
+  beforeEach(function() {
+    bip32e = new Bip32Encryption();
+  });
+
+  describe('makeWallet', function() {
+
+    fixtures.valid.forEach(function(f) {
+
+      it('should make the clear text wallet for root key ' + f.rootKey, function() {
+        var wallet = bip32e.makeWallet(f.rootKey, f.date);
+        assert.equal(wallet, f.clear);
+      });
+    });
+
+    fixtures.invalid.makeWallet.forEach(function(f) {
+      it('should throw ' + f.description, function() {
+        assert.throws(function() {
+          bip32e.makeWallet(f.rootKey, f.creationDate, f.password, "", 0, f.saltEntrpy);
+        }, new RegExp(f.exception));
+      });
+    });
+
+  });
+});
